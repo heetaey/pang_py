93ced690abd2e4a75ef962d5a28ea915c731495d
heetaeyang <htyang90@gmail.com>
2020-06-22 23:30:03 -0700

    Collision logics and breaking balloons to half sizes

--- pang.py
+++ pang.py
@@ -70,6 +70,9 @@ balls = [{
     "init_speed_y": ball_speed_y[0]  # initial speed of y
 }]
 
+weapon_to_remove = -1
+ball_to_remove = -1
+
 # running environment
 running = True
 while running:
@@ -133,6 +136,81 @@ while running:
 
         ball_val["pos_x"] += ball_val["to_x"]
         ball_val["pos_y"] += ball_val["to_y"]
+
+    # collision logics
+    player_rect = player.get_rect()
+    player_rect.left = player_xpos
+    player_rect.top = player_ypos
+
+    for ball_idx, ball_val in enumerate(balls):
+        ball_pos_x = ball_val["pos_x"]
+        ball_pos_y = ball_val["pos_y"]
+        ball_img_idx = ball_val["img_idx"]
+
+        # ball rect update
+        ball_rect = ball_images[ball_img_idx].get_rect()
+        ball_rect.left = ball_pos_x
+        ball_rect.top = ball_pos_y
+
+        # collision detect
+        if player_rect.colliderect(ball_rect):
+            running = False
+            break
+
+        # ball and weapon collision
+        for weapon_idx, weapon_val in enumerate(weapons):
+            weapon_pos_x = weapon_val[0]
+            weapon_pos_y = weapon_val[1]
+
+            weapon_rect = weapon.get_rect()
+            weapon_rect.left = weapon_pos_x
+            weapon_rect.top = weapon_pos_y
+
+            if weapon_rect.colliderect(ball_rect):
+                weapon_to_remove = weapon_idx
+                ball_to_remove = ball_idx
+
+                # if balloon size is not the smallest one
+                if ball_img_idx < 3:
+                    # retreive current ball size info
+                    ball_width = ball_rect.size[0]
+                    ball_height = ball_rect.size[1]
+
+                    small_ball_rect = ball_images[ball_img_idx + 1].get_rect()
+                    small_ball_width = small_ball_rect.size[0]
+                    small_ball_height = small_ball_rect.size[1]
+
+                    # ball bouncing to left
+                    balls.append({
+                            "pos_x": ball_pos_x + (ball_width / 2) - (small_ball_width / 2),
+                            "pos_y": ball_pos_y + (ball_height / 2) - (small_ball_height / 2),
+                            "img_idx": ball_img_idx + 1,
+                            "to_x": -3,  # movement to x-axis
+                            "to_y": -6,  # movement to y-axis
+                            "init_speed_y": ball_speed_y[ball_img_idx + 1]  # initial speed of y
+                        })
+
+                    # ball bouncing to right
+                    balls.append({
+                            "pos_x": ball_pos_x + (ball_width / 2) - (small_ball_width / 2),
+                            "pos_y": ball_pos_y + (ball_height / 2) - (small_ball_height / 2),
+                            "img_idx": ball_img_idx + 1,
+                            "to_x": 3,  # movement to x-axis
+                            "to_y": -6,  # movement to y-axis
+                            "init_speed_y": ball_speed_y[ball_img_idx + 1]  # initial speed of y
+                        })
+
+
+                break
+
+    # remove balls
+    if ball_to_remove > -1:
+        del balls[ball_to_remove]
+        ball_to_remove = -1
+
+    if weapon_to_remove > -1:
+        del weapons[weapon_to_remove]
+        weapon_to_remove = -1
 
     # update sprites
     screen.blit(background, (0, 0))
